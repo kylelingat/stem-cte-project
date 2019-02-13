b2cd20e84ff93f5a082db0a0d7246537d7afdcd3
Kyle <kyle@grace.local>
2019-02-13 10:29:20 -1000

    using react-table to display database data

--- src/db-test/db.js
+++ src/db-test/db.js
@@ -3,45 +3,50 @@ import "./db.css";
 import axios from "axios";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
+var students;
+var data;
 
 export default class DbTest extends Component {
-  componentDidMount() {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            students: students
+        };
+      }
+
+    componentDidMount = () => {
     axios
       .get(`https://v4pq771b89.execute-api.us-west-2.amazonaws.com/dev/get`)
       .then(res => {
-        const persons = res.data.message.rows;
-        console.log(persons);
+        students = res.data.message.rows;
+        this.setState({
+            students: students
+        })
+        console.log(this.state.students);
       });
   }
 
   render() {
-    const data = [
-      {
-        firstName: "Jacob",
-        lastName: "Harris",
-        grade: 9
-      }
-    ];
-
     const columns = [
       {
         Header: "First Name",
-        accessor: "firstName" // String-based value accessors!
+        accessor: "first_name" // String-based value accessors!
       },
       {
         Header: "Last Name",
-        accessor: "lastName"
+        accessor: "last_name"
       },
       {
         Header: "Grade",
-        accessor: "grade"
+        accessor: "grade_level"
       }
     ];
 
     return (
       <div className="dbTestContainer">
         <ReactTable
-          data={data}
+          data={this.state.students}
           columns={columns}
           defaultPageSize={5}
           showPaginationBottom={false}
